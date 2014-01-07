4a00de4e9a196a48d119e83dc34697d836a30615
Jammie1 <jamie.magee@gmail.com>
2014-01-07 01:03:50 +0000

    Problem 004 c++

---
+++ c++/004.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool palindrome(int a)
+{
+  string c = to_string(a), b = c;
+  reverse(c.begin(), c.end());
+  return b == c;
+}
+
+int main()
+{
+  int largest = 0, j, k; 
+  
+  for (int i = 999; i >= 100; i--)
+  {
+    if (i%11==0) 
+    {
+      j = 999;
+      k = 1;
+    }
+    else
+    {
+      j = 990;
+      k == 11;
+    }
+    for (j; j >= i; j-k)
+    {
+      if (i*j <= largest) break;
+      if (palindrome(i*j)) largest = i*j;
+    }
+  }
+  cout << largest << endl;
+}
