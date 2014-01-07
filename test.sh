0c771280baf715365c7312866fe5d3260d05e9a0
Jammie1 <jamie.magee@gmail.com>
2014-01-07 02:12:02 +0000

    Test MATLAB on Travis-CI

--- test.sh
+++ test.sh
@@ -1,1 +1,38 @@
-green='\e[0;32m'; red='\e[1;31m'; endColour='\e[0m'; for x in c++/*.cc; do x=$(echo $x|cut -c 5- |rev| cut -c 4- |rev); make c++ $x > /dev/null; output=$(./c++/$x); answer=$(eval "sed -n '"$x"p' assets/answers.txt"); if [ $output == $answer ]; then echo -e "${green}Test $x passed${endColour}"; else echo "${red}Test $x failed${endColour}"; fi; done
+green='\e[0;32m'
+red='\e[1;31m'
+endColour='\e[0m'
+
+yes | sudo apt-add-repository ppa:octave/stable > /dev/null
+sudo apt-get -qq update
+sudo apt-get -qq install octave > /dev/null
+
+#C++
+echo "C++ Tests"
+for x in c++/*.cc
+do 
+  x=$(echo $x|cut -c 5- |rev| cut -c 4- |rev)
+  make c++ $x > /dev/null
+  output=$(./c++/$x)
+  answer=$(eval "sed -n '"$x"p' assets/answers.txt")
+  if [ $output == $answer ]
+  then 
+    echo -e "${green}Test $x passed${endColour}"
+  else 
+    echo -e "${red}Test $x failed${endColour}"
+  fi
+done
+
+#MATLAB
+echo "MATLAB Tests"
+for x in matlab/*.m
+do
+  output=$(octave -qf --no-window-system $x)
+  x=$(echo $x|cut -c 8- |rev| cut -c 3- |rev)
+  answer="ans =  $(eval "sed -n '"$x"p' assets/answers.txt")"
+  if [ "$output" == "$answer" ]
+  then 
+    echo -e "${green}Test $x passed${endColour}"
+  else 
+    echo -e "${red}Test $x failed${endColour}"
+  fi
+done
