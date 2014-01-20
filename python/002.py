cd74266c1e3dcddd1736e98231b3e375210eb0cd
Jamie Magee <jamie.magee@gmail.com>
2014-01-20 18:48:55 +0000

    Python 005

--- python/002.py
+++ python/002.py
@@ -1,8 +1,9 @@
 a = b = c = 1
 s = 0
-while(c < 4e6):
+while c < 4e6:
     c = a + b
-    if (c % 2 == 0): s += c
+    if c % 2 == 0:
+        s += c
     a = b
     b = c
 
---
+++ python/005.py
@@ -0,0 +1,18 @@
+import math
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+a = [1] * 10
+i = 0
+n = 1
+check = True
+
+while primes[i] <= 20:
+    if check:
+        if primes[i] <= math.sqrt(20):
+            a[i] = math.floor(math.log10(20)/math.log10(primes[i]))
+        else:
+            check = False
+    n *= math.pow(primes[i], a[i])
+    i += 1
+
+print int(n)
