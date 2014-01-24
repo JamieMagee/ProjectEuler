3bd32c49339ab5bc55d0745e7c77aac20c19f8d7
Jamie Magee <jamie.magee@gmail.com>
2014-01-24 22:16:04 +0000

    Python 053

---
+++ python/053.py
@@ -0,0 +1,11 @@
+ans = 0
+for i in range(1, 101):
+    d, n, v = 1, i, 1
+    while n > i/2:
+        v = v*n/d
+        if v > 1000000:
+            ans += (1 + i) - (d << 1)
+            break
+        n -= 1
+        d += 1
+print(ans)
