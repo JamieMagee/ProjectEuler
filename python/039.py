188b1355187a579714489bfb94257bb3a5dd1829
Jamie Magee <jamie.magee@gmail.com>
2014-01-24 20:46:19 +0000

    Python 039

---
+++ python/039.py
@@ -0,0 +1,18 @@
+import math
+
+
+def gcd(a, b):
+    return gcd(b, a % b) if b else a
+
+lim, max = 1000, 0
+count = [0] * lim
+for n in range(1, int(math.sqrt(lim)), 2):
+    for m in range(2, int(math.sqrt(lim)), 2):
+        if gcd(n, m) == 1:
+            sum = 2*m*n+m*m+n*n+abs(m*m-n*n)
+            for s in range(sum, lim, sum):
+                count[s] += 1
+for i in range(1, lim):
+    if count[i] > max:
+        ans, max = i, count[i]
+print(ans)
