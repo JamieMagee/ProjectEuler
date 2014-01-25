11e2af4e9b68bb00e832e744497dc041449c3e9d
Jamie Magee <jamie.magee@gmail.com>
2014-01-25 12:24:47 +0000

    Python 076

---
+++ python/076.py
@@ -0,0 +1,13 @@
+def partition(n):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    if n not in partitions:
+        partitions[n] = sum([(-1) ** (k + 1) * (partition(n - (k * (3 * k - 1) / 2))
+                                                + partition(n - (k * (3 * k + 1) / 2))) for k in range(1, n + 1)])
+    return partitions[n]
+
+
+partitions = {}
+print(partition(100) - 1)
