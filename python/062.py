e2af695a71d815c104c85aa567a872de6fe1acaa
Jamie Magee <jamie.magee@gmail.com>
2014-01-26 17:52:02 +0000

    Python 062

---
+++ python/062.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+
+cubes = defaultdict(list)
+for i in range(10000):
+    digits = ''.join(sorted([d for d in str(i**3)]))
+    cubes[digits].append(str(i**3))
+print min([min(v) for k, v in cubes.items() if len(v) == 5])
