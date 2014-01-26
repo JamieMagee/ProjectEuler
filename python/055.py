1128f9edb305028c36ebde5fe31cab02469d000b
Jamie Magee <jamie.magee@gmail.com>
2014-01-26 21:26:49 +0000

    Python 055

---
+++ python/055.py
@@ -0,0 +1,13 @@
+def ispalindrome(word):
+    return word == word[::-1]
+
+ans = 0
+for i in range(10000):
+    num, iterations = i, 0
+    num += int(str(num)[::-1])
+    while iterations < 50 and not ispalindrome(str(num)):
+        num += int(str(num)[::-1])
+        iterations += 1
+    if not ispalindrome(str(num)):
+        ans += 1
+print(ans)
