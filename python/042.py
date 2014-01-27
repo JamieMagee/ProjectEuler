f90c6d13dc53eaace4b2d60b424da0001eb62d2e
Jamie Magee <jamie.magee@gmail.com>
2014-01-27 00:54:27 +0000

    Python 042

---
+++ python/042.py
@@ -0,0 +1,17 @@
+from math import sqrt, floor
+
+
+def istri(n):
+    return floor(sqrt(8*n+1)) == sqrt(8*n+1)
+
+
+def sumchar(s):
+    return sum(ord(c)-64 for c in s)
+
+
+ans = 0
+words = eval('[' + open('../assets/042.txt').readlines()[0] + ']')
+for word in words:
+    if istri(sumchar(word)):
+        ans += 1
+print(ans)
