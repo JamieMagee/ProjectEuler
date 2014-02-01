675df0b81ac0ef1efa73e2ce656f45e514357d32
Jamie Magee <jamie.magee@gmail.com>
2014-02-01 00:27:08 +0000

    Python 044

---
+++ python/044.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+from operator import *
+
+
+def pentgen(n):
+    pent = set()
+    for i in range(1, n):
+        pent.add(i*(3*i-1)/2)
+    return pent
+
+
+pent = pentgen(3000)
+c = combinations(pent, 2)
+for p in c:
+    if add(*p) in pent and abs(sub(*p)) in pent:
+        print(abs(sub(*p)))
